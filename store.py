1981e6e021914da7f1fe0161bc23b82ce42c1fd7
sumsar01 <skriver@live.dk>
2021-04-20 10:58:00 +0200

    Uploading main and functions
    Creating a the main store file and the store_function file.

---
+++ store.py
@@ -0,0 +1,55 @@
+import store_function
+
+shop = {
+    "inventory": 
+        {
+        123: ["Kartofler", 4.75, "kg", 100],
+        143: ["Makrelguf", 10.50, "pakke", 30],
+        185: ["Fintmalet kaffe", 44.25, "stk", 40]
+        },
+
+    "cash" : 
+        588.75,
+
+    "receipts": 
+        [],
+
+    "employees":
+        ["Michael", "Rasmus"]
+    }
+
+
+
+
+
+
+"""
+done = False
+while not done:
+    print("Velkommen til næste kunde...")
+    sum = 0.0
+
+    flere_varer = True
+    while flere_varer:
+        stregkode = input("indtast varer nr: ")
+        if stregkode == "":
+            flere_varer = False
+        else:
+            stregkode = int(stregkode)
+            if stregkode in varelager:
+                print("Varen er {} og prisen er {} kr, der er {} enheder på lager".format(varelager[stregkode][0], varelager[stregkode][1], varelager[stregkode][3]))
+                antal = int(input("Hvor mange enheder købes? "))
+                if antal == 0:
+                    break
+                varelager[stregkode][3] -= antal
+                sum += int(varelager[stregkode][1])*antal
+            else:
+                print("Fejl stregkode findes ikke...")
+            
+
+    print("Det bliver {} kr".format(sum))
+    svar = input("Er der flere kunder? y/n: ")
+    if svar == 'n':
+        done = True
+        print("Der er ikke flere kunder...")
+"""
---
+++ store_function.py
@@ -0,0 +1,43 @@
+
+
+def open_shop(shop):
+    #optæl kasse
+    #optæl varebeholdning
+    #ny bong
+    pass
+
+def close_shop(shop):
+    #optæl kasse
+    #optæl bonner
+    #bestil nye varer
+    #afstæm kasse
+    #udskriv kasse raport
+    pass
+
+def sale():
+    #for alle varer i salget:
+    ##fjern vare fra lager
+    ##læg priser til sum
+    ##opdater bonner
+    #udskriv bon
+    #modtag betaling til kassen
+    #fejl beskeder for varer som ikke findes
+    pass
+
+def supply():
+    #tilføj varer
+    #fjern varer
+    #se lager
+    #opdater lager
+    #betal for lager
+    #købsnota
+    pass
+
+
+
+
+
+
+
+
+
