09361e643b3b6c48a3f4d98b54a4b24d8cd83944
sumsar01 <skriver@live.dk>
2021-04-20 13:03:11 +0200

    Finishing supply()
    Finishing the supply function.

--- store_function.py
+++ store_function.py
@@ -32,60 +32,65 @@ supply_options = """
     0) Exit
                 """
 
-
-shop = {
-    "inventory": 
-        {
-        123: ["Kartofler", 4.75, "kg", 100],
-        143: ["Makrelguf", 10.50, "pakke", 30],
-        185: ["Fintmalet kaffe", 44.25, "stk", 40]
-        },
-
-    "cash" : 
-        588.75,
-
-    "receipts": 
-        [],
-
-    "employees":
-        ["Michael", "Rasmus"]
-    }
-
-
 def supply(shop):
+    """Enters supply options.
+    1) Allows user to add item to inventory under item IDs
+    2) Allows user to remove items using item IDs
+    3) Show user current item stock
+    4) Allows user to restock items by using their ID and number of added items
+
+
+    """
+
     done = False
     while not done:
         print(supply_options)
         option = input("Chose an option: ")
+
+        # Add item
         if option == "1":
             print("Adding new sales item...")
             item_ID = input("Item ID: ")
-            item_name = input("Item name: ")
-            item_price = float(input("Item price: "))
-            item_unit = input("What is the unit of the item?: ")
-            item = {item_ID: [item_name, item_price, item_unit, 0]}
-            shop["inventory"].extend(item)
+            if item_ID in shop["inventory"]:
+                print("Error: ID already exist in inventory...")
+            else:
+                item_name = input("Item name: ")
+                item_price = float(input("Item price: "))
+                item_unit = input("What is the unit of the item?: ")
+                item = {item_ID: [item_name, item_price, item_unit, 0]}
+                shop["inventory"].update(item)
 
+        # Remove item
         if option == "2":
-            pass
+            print("Removing item from stock...")
+            item_ID = input("Item ID: ")
+            if item_ID in shop["inventory"]:
+                del shop["inventory"][item_ID]
+            else:
+                print("Error: Item ID not found...")
 
+        # View supply
         if option == "3":
-            pass
+            print("{:10}{:20}{:7}{:10}{:10}".format("Item ID", "Name", "Price", "Units", "Stock"))
+            for item in shop["inventory"]:
+                print("{:7}   {:20}{:5}  {:5} {:7}".format(item, shop["inventory"][item][0], shop["inventory"][item][1], shop["inventory"][item][2], shop["inventory"][item][3]))
 
+        # Update supply
         if option == "4":
-            print("{}{}{}{}".format("Item ID", "Name", ))
+            print("Updation store...")
+            item_ID = int(input("Item ID: "))
+            if item_ID not in shop["inventory"]:
+                print("Error: Item ID not found...")
+            else:
+                print(f'{item_ID} is {shop["inventory"][item_ID][0]} currently supply {shop["inventory"][item_ID][3]} in stock...')
+                updated_stock = int(input("How many would you like to add?: "))
+                shop["inventory"][item_ID][3] += updated_stock
+                print(f'{item_ID} is {shop["inventory"][item_ID][0]} updated supply {shop["inventory"][item_ID][3]} in stock...')
 
+        # Exit supply
         if option == "0":
-            pass
-    #tilføj varer
-    #fjern varer
-    #se lager
-    #opdater lager
-    #betal for lager
-    #købsnota
-
-
-supply(shop)
+            print("Exiting supply options...")
+            done = True
 
 
 
