1c9ad0b2a9ccde16131b854b0553327f256da234
MichaelHR1992 <michaelhr@outlook.dk>
2021-04-20 13:10:55 +0200

    Two changes
    English change in open_shop. And started on close_shop

---
+++ close_shop.py
@@ -0,0 +1,9 @@
+def close_shop(shop):
+    #optæl kasse
+    print(f"Kassen har {shop['cash']} penge")
+    #optæl bonner
+    
+    #bestil nye varer
+    #afstæm kasse
+    #udskriv kasse raport
+    pass
--- open_shop.py
+++ open_shop.py
@@ -1,14 +1,13 @@
 
 def open_shop(shop):
-    """Denne funktion optæller kassen, varebeholdningen og udskriver disse. 
-    Til sidst laves en ny bon."""
+    """This function counts the register, stash and prints these. Lastly, it makes a new receipt."""
 
     #optæl kasse
-    print(f"Kassen har {shop['cash']} penge")
+    print(f"Register contains {shop['cash']} money")
     #optæl varebeholdning
-    print("Varelageret indeholder:\n")
+    print("Stash contains:\n")
     for key in shop["inventory"]:
-        print(f"Vare: {shop['inventory'][key][0]}    Mængde: {shop['inventory'][key][3]}")
+        print(f"Item: {shop['inventory'][key][0]}    Amount: {shop['inventory'][key][3]}")
     #ny bong
     print("Making a new and empty receipt for the day.")
     shop["receipts"].clear()
