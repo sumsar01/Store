07905e38fc923b352b79792c7e4df5e44c625a0d
MichaelHR1992 <michaelhr@outlook.dk>
2021-04-27 14:47:32 +0200

    book v2

--- michael_work.py
+++ michael_work.py
@@ -13,26 +13,32 @@ class Book:
     def add_reciepts(self, file):
         pass
 
-    def find_total_in(self, reciepts, date):
-        pass
+    def find_day_costs(self, date):
+        day_costs = 0 
+        reciepts_of_day_costs = list(filter(lambda e: e[0].date() == date and e[2] < 0, self.reciepts))
+        for i in range(len(reciepts_of_day_costs)):
+            day_costs += reciepts_of_day_costs[i][2] 
+        self.day_costs.append(day_costs)
 
-    def find_day_sales(self, day):
+    def find_day_sales(self, date):
         day_sales = 0 
-        reciepts_of_day_sales = list(filter(lambda e: e[0].day == day and e[2] > 0, self.reciepts))
+        reciepts_of_day_sales = list(filter(lambda e: e[0].date() == date and e[2] > 0, self.reciepts))
         for i in range(len(reciepts_of_day_sales)):
             day_sales += reciepts_of_day_sales[i][2] 
         self.day_sales.append(day_sales)
 
     def print_history(self):
-        unique_days_in_reciepts = np.unique([e[0].date.day for e in self.reciepts])
+        unique_days_in_reciepts = np.unique([e[0].date() for e in self.reciepts])
         print("{:<10} {:<10} {:<10}".format("Sales", "Costs", "Date"))
         for i in range(len(self.day_sales)):
             print("{:<10} {:<10} {:<10}".format(self.day_sales[i], self.day_costs[i], unique_days_in_reciepts[i]))
 
 book = Book()
-book.reciepts = [(dt.datetime.now(), [[1, 10, 40, 50], [2, 10, 30, 50], [3, 40, 10, 20]], 20), (dt.datetime.now(), [[1, 10, 40, 50], [2, 10, 30, 50], [3, 40, 10, 20]], 20)]
-book.find_day_sales(dt.datetime.now().day)
-print(book.day_sales)
+book.reciepts = [(dt.datetime.now(), [[1, 10, 40, 50], [2, 10, 30, 50], [3, 40, 10, 20]], 20), (dt.datetime.now(), [[1, 10, 40, 50], [2, 10, 30, 50], [3, 40, 10, 20]], 20),
+    (dt.datetime.now(), [[1, 30, 40, 0], [2, 20, 30, 0], [5, 40, 10, 0]], -30)]
+book.find_day_sales(dt.datetime.now().date())
+book.find_day_costs(dt.datetime.now().date())
+print(book.day_costs)
 
 
 #book.day_sales = [10, 20]
