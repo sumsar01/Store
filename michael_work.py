3cf1375dac041866e28608b4d7819fdd96c898f4
MichaelHR1992 <michaelhr@outlook.dk>
2021-05-03 10:55:02 +0200

    Reciept ændringer
    Rettede nogle steder som manglede self

--- michael_work.py
+++ michael_work.py
@@ -2,55 +2,42 @@ import numpy as np
 import datetime as dt
     
 class Reciept:
-    def __init__(self, list_of_id_quantity, buy_sell="s"):
+    def __init__(self, list_of_id_quantity, stock, purchase_sale="s"):
         self.date = dt.datetime.now().date()
         self.item_entries = []
         self.total = 0
-        self.buy_sell = buy_sell
+        self.purchase_sale = purchase_sale
 
-        prices_or_costs = calc_item_pricecost(list_of_id_quantity)
-        self.item_entries = make_final_item_entries(list_of_id_quantity, prices_or_costs)
-        calc_total()
+        prices_or_costs = self.calc_item_pricecost(list_of_id_quantity, stock)
+        self.item_entries = self.make_full_item_entries(list_of_id_quantity, prices_or_costs)
+        self.calc_total()
 
 
-    def calc_item_pricecost(self):
-        if self.buy_sell == "s":
-            # fetch prices of IDs (brug stock)
-            # make list with all prices of IDs
-            # multiply unit prices with quantities to get prices
-            break
-        if self.buy_sell == "b":
-            # fetch costs of IDs (brug stock)
-            # make list with costs of IDs
-            # multiply unit costs with quantities to get costs
-            break
-        pass
+    def calc_item_pricecost(self, list_of_id_q, stock):
+        prices_costs = []
+        for ID, quantity in list_of_id_q:
+            price_cost = 0
+            if self.purchase_sale == "s":
+                price_cost = stock.get_price(ID)
+            elif self.purchase_sale == "p":
+                price_cost = stock.get_cost(ID)
+            else:
+                ValueError("purchase_sale need to be 's' or 'p'...")
+
+            prices_costs.append(price_cost * quantity)
+        return prices_costs
 
             
-    def make_full_item_entries():
-
-        pass
+    def make_full_item_entries(self, list_of_id_quantity, prices_or_costs):
+        item_entries_list = []
+        for i in range(len(prices_or_costs)):
+            id = list_of_id_quantity[i][0]
+            quantity = list_of_id_quantity[i][1]
+            item_entries_list.append((id, quantity, prices_or_costs[i]))
+        return item_entries_list
     
-    def calc_total():
-        pass
-
-
-
-        for ID, amount, 
-
-
-
-
-book = Book()
-book.reciepts = [(dt.datetime.now(), [[1, 10, 40, 50], [2, 10, 30, 50], [3, 40, 10, 20]], 20), (dt.datetime.now(), [[1, 10, 40, 50], [2, 10, 30, 50], [3, 40, 10, 20]], 20),
-    (dt.datetime.now(), [[1, 30, 40, 0], [2, 20, 30, 0], [5, 40, 10, 0]], -30)]
-book.find_day_sales(dt.datetime.now().date())
-book.find_day_costs(dt.datetime.now().date())
-print(book.day_costs)
-
-
-#book.day_sales = [10, 20]
-#book.day_costs = [10, 20]
-#book.day_balance = [0, 0]
-
-#book.print_history()
+    def calc_total(self):
+        total = 0
+        for i in range(len(self.item_entries)):
+            total += self.item_entries[i][2]
+        return total
