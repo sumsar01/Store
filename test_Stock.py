171db57e5bdda80b7d61d00d9e5c5ca1c0ff8470
sumsar01 <skriver@live.dk>
2021-05-03 12:59:24 +0200

    made test file for stock

---
+++ test_Stock.py
@@ -0,0 +1,35 @@
+import unittest
+import shop
+
+class Stock_test(unittest.TestCase):
+
+    def setUp(self):
+        self.stock1 = shop.Stock()
+        self.stock2 = shop.Stock()
+
+    def test_add_item(self):
+        pass
+
+    def test_use_item(self):
+        pass
+
+    def test_update_stock(self):
+        pass
+
+    def test_delete_item(self):
+        pass
+
+    def test_stock_information(self):
+        pass
+
+    def test_get_price(self):
+        pass
+
+    def test_get_cost(self):
+        pass
+
+    def test_get_item(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
